6cb92caf9b101e4a0a775e97810d118938cecebc
chikara-k <wabisabitea.c@gmail.com>
2025-12-08 10:46:29 +0900

    Palindrome Linked List2

--- leetCode/palindromeLinkedList.go
+++ leetCode/palindromeLinkedList.go
@@ -22,3 +22,27 @@ func isPalindrome(head *ListNode) bool {
 
     return true
 }
+
+// Other Answer
+func isPalindrome(head *ListNode) bool {
+    fast, slow := head, head
+    for fast != nil && fast.Next != nil {
+        fast = fast.Next.Next
+        slow = slow.Next
+    }
+    var prev *ListNode
+    curr := slow
+    for curr != nil {
+        next := curr.Next
+        curr.Next = prev
+        prev = curr
+        curr = next
+    }
+    for prev != nil && head != nil {
+        if prev.Val != head.Val {
+            return false
+        }
+        prev, head = prev.Next, head.Next
+    }
+    return true
+}
