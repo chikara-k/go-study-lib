3ef6ce5ba42ee2e370b738142812b40de81b7ce5
chikara-k <wabisabitea.c@gmail.com>
2025-12-09 09:35:13 +0900

    refactor

--- leetCode/palindromeLinkedList.go
+++ leetCode/palindromeLinkedList.go
@@ -1,48 +1,51 @@
+package leetcode
+
 /**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-func isPalindrome(head *ListNode) bool {
-    vals := []int{}
-    for cur := head; cur != nil; cur = cur.Next {
-        vals = append(vals, cur.Val)
-    }
-
-    l, r := 0, len(vals)-1
-    for l < r {
-        if vals[l] != vals[r] {
-            return false
-        }
-        l++
-        r--
-    }
-
-    return true
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
+
+/**
+func isPalindrome(head *ListNode) bool {
+	vals := []int{}
+	for cur := head; cur != nil; cur = cur.Next {
+		vals = append(vals, cur.Val)
+	}
+
+	l, r := 0, len(vals)-1
+	for l < r {
+		if vals[l] != vals[r] {
+			return false
+		}
+		l++
+		r--
+	}
+
+	return true
+}
+*/
 
 // Other Answer
 func isPalindrome(head *ListNode) bool {
-    fast, slow := head, head
-    for fast != nil && fast.Next != nil {
-        fast = fast.Next.Next
-        slow = slow.Next
-    }
-    var prev *ListNode
-    curr := slow
-    for curr != nil {
-        next := curr.Next
-        curr.Next = prev
-        prev = curr
-        curr = next
-    }
-    for prev != nil && head != nil {
-        if prev.Val != head.Val {
-            return false
-        }
-        prev, head = prev.Next, head.Next
-    }
-    return true
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	var prev *ListNode
+	curr := slow
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	for prev != nil && head != nil {
+		if prev.Val != head.Val {
+			return false
+		}
+		prev, head = prev.Next, head.Next
+	}
+	return true
 }
--- leetCode/reverseLinkedList.go
+++ leetCode/reverseLinkedList.go
@@ -1,10 +1,10 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+package leetcode
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
 func reverseList(head *ListNode) *ListNode {
     var prev *ListNode
     cur := head
