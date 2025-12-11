d9601ddf62f1bb152f63f2d140f1688d026035b8
chikara-k <wabisabitea.c@gmail.com>
2025-12-11 09:30:52 +0900

    Contains Duplicate

---
+++ leetCode/containsDuplicate.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func containsDuplicate(nums []int) bool {
+    seen := map[int]bool{}
+
+    for _, n := range nums {
+        if seen[n] {
+            return true
+        }
+        seen[n] = true
+    }
+    return false
+}
